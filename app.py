a51bde32a9a6d4e753c5ec228b386e3e794d715b
TrueValPoperty <williamtylerstreet@gmail.com>
2025-07-02 21:42:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
 import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
-def home():
-    return 'API Home'
+def index():
+    return send_from_directory('.', 'index.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
